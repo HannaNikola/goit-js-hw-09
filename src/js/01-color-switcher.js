99479c23d0eed28857960e32a33bd2853dce22db
HannaNikola <h.nikolaienko32@gmail.com>
2023-08-03 22:18:53 +0200

    add-library

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,5 @@
+const buttonStart = document.querySelector("button[data-start]");
+const buttonStop = document.querySelector('button[data-stop]');
 
+console.log(buttonStop);
+console.log('hello')
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,20 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+
+// const formEl = document.querySelector('.form');
+// const buttonSubmit = document.querySelector('button[submit]')
+
+// console.log(formEl);
+
+
+
+
+
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
